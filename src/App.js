b760f30f16e210d04de33ace401a838ca53bbad7
sksamiii <128906151+sksamiii@users.noreply.github.com>
2024-12-04 23:57:26 +0530

    Frontend_react_part

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from "./component/header/Header.jsx";
+import Footer from './component/footer/Footer.jsx';
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
